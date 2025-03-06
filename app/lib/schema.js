59e3c32a8137dfbd97fbfa2d3c0b1ea32d1ece69
Aluminatix <p4as3nj85@gmail.com>
2025-03-06 21:43:47 +0530

    all finished

--- app/lib/schema.js
+++ app/lib/schema.js
@@ -7,8 +7,7 @@ export const accountSchema = z.object({
   isDefault: z.boolean().default(false),
 });
 
-export const transactionSchema = z
-  .object({
+export const transactionSchema = z.object({
     type: z.enum(["INCOME", "EXPENSE"]),
     amount: z.string().min(1, "Amount is required"),
     description: z.string().optional(),
@@ -16,8 +15,8 @@ export const transactionSchema = z
     accountId: z.string().min(1, "Account is required"),
     category: z.string().min(1, "Category is required"),
     isRecurring: z.boolean().default(false),
-    recurringInterval: z
-      .enum(["DAILY", "WEEKLY", "MONTHLY", "YEARLY"])
+    recurringInterval: 
+      z.enum(["DAILY", "WEEKLY", "MONTHLY", "YEARLY"])
       .optional(),
   })
   .superRefine((data, ctx) => {
